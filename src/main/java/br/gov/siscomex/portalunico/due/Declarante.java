3245a2157af9a713e5743fd4974866d2339d62fb
Samuel Facchinello <samuelfac@gmail.com>
2017-09-27 14:06:03 -0300

    Adicionado retorno da Consulta DUE

---
+++ src/main/java/br/gov/siscomex/portalunico/due/Declarante.java
@@ -0,0 +1,60 @@
+
+package br.gov.siscomex.portalunico.due;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "numero",
+    "tipo"
+})
+public class Declarante implements Serializable
+{
+
+    @JsonProperty("numero")
+    private String numero;
+    @JsonProperty("tipo")
+    private String tipo;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 4302290545820322624L;
+
+    @JsonProperty("numero")
+    public String getNumero() {
+        return numero;
+    }
+
+    @JsonProperty("numero")
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @JsonProperty("tipo")
+    public String getTipo() {
+        return tipo;
+    }
+
+    @JsonProperty("tipo")
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
